4469c51ffd9efd4567ba571dc5d1a13fbaad7529
keyserwood <elouan.raymond@etu.emse.fr>
2020-11-14 09:05:08 +0100

    first commit

---
+++ test_script.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# 1. Effectuez toutes les opérations détaillées ci-dessous. 
+# 2. Ces modules peuvent vous aidez mais si vous voulez/pouvez faire sans c'est possible aussi.
+# 3. Vous pouvez utiliser internet sans limite, ce ne sera pas pénalisé.
+# 4. Si vous êtes bloqué par un détail technique, n'hésitez pas à poser des questions.
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import sklearn
+
+data_path = "/media/ubuntu/Data/RH_TEST/Data/creditcard.csv"
+
+###############################################################################
+# 1. CHARGER LES DONNEES DISPONIBLES SUR data_path ############################
+###############################################################################
+
+
+###############################################################################
+# 2. VISUALISER LA VARIABLE target ############################################
+###############################################################################
+
+
+###############################################################################
+# 3. ANALYSER LES CORRELATIONS ENTRE features & target ########################
+###############################################################################
+
+
+###############################################################################
+# 4.1. SEPARER LES DONNEES ENTRE variables explicatives & cible AINSI #########
+# 4.2. QU'EN train_set ET test_set ############################################
+###############################################################################
+
+
+###############################################################################
+# 5.1. LANCER UN MODELE SIMPLE DE CLASSIFICATION SUR LES DONNEES ##############
+# 5.2. LES PREDICTION SONT ATTENDUES DANS UNE VARIABLE "y_pred" ###############
+###############################################################################
+
+
+###############################################################################
+# 6. TESTER VOTRE MODELE AVEC UNE METRIQUE DE VOTRE CHOIX #####################
+###############################################################################
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
